958c8a431c1e71a73c5c25db9951c17ecc86e305
Sergey Shimansky <sergeyshimansky@MacBook-Air-Sergey.local>
2017-10-09 19:15:49 +0300

    Доработан AJAX-загрузчик для статей и боковых блоков

--- js/common-loader.js
+++ js/common-loader.js
@@ -1,12 +1,23 @@
 function CommonLoader() {
     var handlerAddress = "http://fazarosta.com/add/";
-    this.makeRequest = function(value) {
+    this.makeRequest = function(incomingObject) {
         "use strict";
         var XHR = new XMLHttpRequest();
-        var data = "page=" + encodeURIComponent(value);
+        var container = incomingObject.container;
+        var data = "";
+        //Обработка объекта в зависимости от наличия в нём определённых свойств;
+        if (incomingObject.page) data = "page=" + encodeURIComponent(incomingObject.page);
+        if (incomingObject.module) data = "module=" + encodeURIComponent(incomingObject.module);
         XHR.onreadystatechange = function() {
+            var response = new Object();
             if (XHR.readyState === 4) {
-                
+                if (XHR.status === 200) {
+                    response = JSON.parse(XHR.responseText);
+                    if (response.status) {
+                        //Добавление содержимого в указанный контейнер (основной или дополнительный);
+                        container.innerHTML += response.content;
+                    } else console.error("К сожалению, возникла ошибка при непосредственном формировании HTML-разметки;");
+                } else console.error("К сожалению, возникла ошибка при получении AJAX-ответа;");
             }
         };
         XHR.open("POST", handlerAddress, true);
