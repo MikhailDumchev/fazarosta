2a47c714954e6bf8db2fe8dc13a859d302614dce
Sergey Shimansky <sergeyshimansky@MacBook-Air-Sergey.local>
2017-10-12 18:48:29 +0300

    В POST-запросы добавлены GET-переменные и изменён адрес PHP-обработчика

--- js/common-loader.js
+++ js/common-loader.js
@@ -1,22 +1,31 @@
 function CommonLoader() {
-    var handlerAddress = "http://fazarosta.com/add/index.php";
     this.makeRequest = function(incomingObject) {
         "use strict";
         var XHR = new XMLHttpRequest();
+        var handlerAddress = "http://partner.fazarosta.com/wp-admin/admin-ajax.php";
         var container = incomingObject.container;
         var data = "";
         //Обработка объекта в зависимости от наличия в нём определённых свойств;
-        if (incomingObject.page) data = "page=" + encodeURIComponent(incomingObject.page);
-        if (incomingObject.module) data = "module=" + encodeURIComponent(incomingObject.module);
-        if (incomingObject.search) data = "search=" + encodeURIComponent(incomingObject.search);
+        if (incomingObject.page) {
+            data = "page=" + encodeURIComponent(incomingObject.page);
+            handlerAddress += "?action=get_posts_html";
+        }
+        if (incomingObject.module) {
+            data = "module=" + encodeURIComponent(incomingObject.module);
+            handlerAddress += "?action=get_posts_sidebar";
+        }
+        if (incomingObject.s) {
+            data = "s=" + encodeURIComponent(incomingObject.s);
+            handlerAddress += "?action=get_post_search";
+        }
         XHR.onreadystatechange = function() {
             var response = new Object();
             if (XHR.readyState === 4) {
                 if (XHR.status === 200) {
                     response = JSON.parse(XHR.responseText);
-                    if (response.status) {
+                    if (response.success) {
                         //Добавление содержимого в указанный контейнер (основной или дополнительный);
-                        container.insertAdjacentHTML("beforeend", response.content);
+                        if (response.data.html) container.insertAdjacentHTML("beforeend", response.data.html);
                         if (incomingObject.callback) {
                             incomingObject.callback.startAnimation();
                         }
@@ -27,5 +36,5 @@ function CommonLoader() {
         XHR.open("POST", handlerAddress, true);
         XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         XHR.send(data);
-    };
+    }
 }
